606794b361c378b2eec044a9654665c5a452a7ca
Dking08 <dastageer.siddiqui@gmail.com>
2024-02-06 20:51:26 +0530

    leetcode question

--- Main/lab8/leet.c
+++ Main/lab8/leet.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 int main(){
-	 int nums;
-	 printf("Enter the number: ");
-	 scanf("%d",&nums);
-	 for(int i=1;i<=(2*nums-1);i++){
-	 	for(int j=1;j<=(2*nums-1);j++){
-	 		if(j%2==0){continue;}
-	 		if(j%2!=0){
-	 	
-	 		}
-	 	}
+	 int Nrow;
+	 printf("Enter the number of rows: ");
+	 scanf("%d",&Nrow);
+	 int icomp,jcomp,min;
+	 for(int i=1;i<=(2*Nrow-1);i++){
+	 	for(int j=1;j<=(2*Nrow-1);j++){
+			icomp = (i>=Nrow)?((i-1)-2*(i-Nrow))%Nrow:(i-1)%Nrow;
+			jcomp = (j>=Nrow)?((j-1)-2*(j-Nrow))%Nrow:(j-1)%Nrow;
+			min = (icomp>=jcomp)?jcomp:icomp;
+			printf("%d ", (Nrow) - min );
+		}
+		printf("\n");
 	 }
 return 0;
 }
